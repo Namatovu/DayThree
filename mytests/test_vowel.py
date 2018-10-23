ce02b5572708f01366bd3d87d8db057fc9b79dd9
Namatovu <namatovudamalie02@gmail.com>
2018-10-22 22:59:57 -0700

    add more tests

--- mytests/test_vowel.py
+++ mytests/test_vowel.py
@@ -4,6 +4,7 @@ from vowelapp.vowel import vowel
 class VowelTest(unittest.TestCase):
     def test_vowel(self):
         self.assertEqual(vowel("dahdah"), (['a'], 3))
+        self.assertEqual(vowel("damdamdam"), (['a'], 6))
 
 if __name__ == "__main__":
     unittest.main()
